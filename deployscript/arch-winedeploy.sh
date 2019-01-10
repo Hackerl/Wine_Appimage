f7739b856fffc9eeee315e4bcd0400a861191333
Hackerl <490021209@qq.com>
2019-01-10 15:12:02 +0800

    change#some

--- deployscript/arch-winedeploy.sh
+++ deployscript/arch-winedeploy.sh
@@ -6,7 +6,7 @@ pacman -Syy
 pacman -S --noconfirm wget file pacman-contrib
 
 # Get Wine
-wget -c https://www.playonlinux.com/wine/binaries/linux-x86/PlayOnLinux-wine-3.10-linux-x86.pol
+wget -nv -c https://www.playonlinux.com/wine/binaries/linux-x86/PlayOnLinux-wine-3.10-linux-x86.pol
 tar xfj PlayOnLinux-wine-*-linux-x86.pol wineversion/
 
 wineworkdir=(wineversion/*)
@@ -15,8 +15,8 @@ cd $wineworkdir
 # Add a dependency library, such as freetype font library
 dependencys=$(pactree -s -u wine |grep lib32 | xargs)
 
-wget -c https://github.com/Hackerl/Wine_Appimage/releases/download/v0.9/libhookexecv.so -O bin/libhookexecv.so
-wget -c https://github.com/Hackerl/Wine_Appimage/releases/download/v0.9/wine-preloader_hook -O bin/wine-preloader_hook
+wget -nv -c https://github.com/Hackerl/Wine_Appimage/releases/download/v0.9/libhookexecv.so -O bin/libhookexecv.so
+wget -nv -c https://github.com/Hackerl/Wine_Appimage/releases/download/v0.9/wine-preloader_hook -O bin/wine-preloader_hook
 
 chmod +x bin/wine-preloader_hook
 
@@ -32,7 +32,7 @@ rm -rf cache
 # appimage
 cd -
 
-wget -c "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage" -O  appimagetool.AppImage
+wget -nv -c "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage" -O  appimagetool.AppImage
 chmod +x appimagetool.AppImage
 
 cat > AppRun <<\EOF
--- deployscript/debian-winedeploy.sh
+++ deployscript/debian-winedeploy.sh
@@ -5,15 +5,15 @@ apt update
 apt install -y aptitude wget file bzip2
 
 # Get Wine
-wget -c https://www.playonlinux.com/wine/binaries/linux-x86/PlayOnLinux-wine-3.10-linux-x86.pol
+wget -nv -c https://www.playonlinux.com/wine/binaries/linux-x86/PlayOnLinux-wine-3.10-linux-x86.pol
 tar xfj PlayOnLinux-wine-*-linux-x86.pol wineversion/
 
 wineworkdir=(wineversion/*)
 cd $wineworkdir
 
 # Add a dependency library, such as freetype font library
-wget -c https://github.com/Hackerl/Wine_Appimage/releases/download/v0.9/libhookexecv.so -O bin/libhookexecv.so
-wget -c https://github.com/Hackerl/Wine_Appimage/releases/download/v0.9/wine-preloader_hook -O bin/wine-preloader_hook
+wget -nv -c https://github.com/Hackerl/Wine_Appimage/releases/download/v0.9/libhookexecv.so -O bin/libhookexecv.so
+wget -nv -c https://github.com/Hackerl/Wine_Appimage/releases/download/v0.9/wine-preloader_hook -O bin/wine-preloader_hook
 
 chmod +x bin/wine-preloader_hook
 
@@ -29,7 +29,7 @@ rm -rf $pkgcachedir
 # appimage
 cd -
 
-wget -c "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage" -O  appimagetool.AppImage
+wget -nv -c "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage" -O  appimagetool.AppImage
 chmod +x appimagetool.AppImage
 
 chmod +x AppRun
